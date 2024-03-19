9be7b8e95472609aa47513cb9dde270e0a5b1746
Miquel Fàbrega Pascual <120173496+miquelfabregapascual@users.noreply.github.com>
2024-03-19 10:51:30 +0100

    Add files via upload

---
+++ TamagochiGame/TamagochiGame/Critter.h
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef CRITTER_H
+#define CRITTER_H
+
+#include <string>
+
+class Critter {
+public:
+    // Constructor
+    Critter(std::string name, int hunger, int happiness, int energy);
+
+    // Methods
+    void feed(int amount);
+    void play(int time);
+    void talk();
+    std::string getPhrase();
+    void sleep(int hours);
+    void passTime(int hours);
+    void printStats();
+    void playGuessingGame(); // New method for mini-game
+
+    // Getters
+    int getMood() const;
+    int getEnergy() const;
+    int getHunger() const;
+
+private:
+    std::string name;
+    int hunger;
+    int happiness;
+    int energy;
+};
+
+#endif
+
+
